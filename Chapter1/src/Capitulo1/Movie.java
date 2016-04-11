7448f9cdc0aa50fa61f74b7e36f0bf0a60c14cc4
jorge <jmc766@inlumine.ual.es>
2016-04-11 18:18:30 +0200

    Refactoring 12

--- Chapter1/src/Capitulo1/Movie.java
+++ Chapter1/src/Capitulo1/Movie.java
@@ -8,17 +8,32 @@ public class Movie {
 	private int _priceCode;
 
 	public Movie(String name, int priceCode) {
-		 _name = name;
-		 setPriceCode(priceCode);
-		 }
+		_name = name;
+		setPriceCode(priceCode);
+	}
 
 	public int getPriceCode() {
-		return _priceCode;
+		return _price.getPriceCode();
 	}
 
 	public void setPriceCode(int arg) {
-		_priceCode = arg;
+		switch (arg) {
+		case REGULAR:
+			_price = new RegularPrice();
+			break;
+		case CHILDRENS:
+			_price = new ChildrensPrice();
+			break;
+		case NEW_RELEASE:
+			_price = new NewReleasePrice();
+			break;
+		default:
+			throw new IllegalArgumentException("Incorrect Price Code");
+		}
 	}
+
+	private Price _price;
+
 
 	public String getTitle() {
 		return _name;
