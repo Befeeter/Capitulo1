97e9179a8246025af0a7671d4a98ecd1b6679f7e
jorge <jmc766@inlumine.ual.es>
2016-04-11 17:49:40 +0200

    Refactoring 2

--- Chapter1/src/Capitulo1/Rental.java
+++ Chapter1/src/Capitulo1/Rental.java
@@ -1,4 +1,5 @@
 package Capitulo1;
+
 class Rental {
 	private Movie _movie;
 	private int _daysRented;
@@ -14,5 +15,25 @@ class Rental {
 
 	public Movie getMovie() {
 		return _movie;
+	}
+
+	double getCharge() {
+		double result = 0;
+		switch (getMovie().getPriceCode()) {
+		case Movie.REGULAR:
+			result += 2;
+			if (getDaysRented() > 2)
+				result += (getDaysRented() - 2) * 1.5;
+			break;
+		case Movie.NEW_RELEASE:
+			result += getDaysRented() * 3;
+			break;
+		case Movie.CHILDRENS:
+			result += 1.5;
+			if (getDaysRented() > 3)
+				result += (getDaysRented() - 3) * 1.5;
+			break;
+		}
+		return result;
 	}
 }
