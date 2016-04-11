2f67d87fc5fee3b9d1ec7b3ce9dfb96b9e17f368
jorge <jmc766@inlumine.ual.es>
2016-04-11 18:38:11 +0200

    Refactoring 16

--- Chapter1/src/Capitulo1/Price.java
+++ Chapter1/src/Capitulo1/Price.java
@@ -3,24 +3,12 @@ package Capitulo1;
 abstract class Price {
 	abstract int getPriceCode();
 
-	double getCharge(int daysRented) {
-		double result = 0;
-		switch (getPriceCode()) {
-		case Movie.REGULAR:
-			result += 2;
-			if (daysRented > 2)
-				result += (daysRented - 2) * 1.5;
-			break;
-		case Movie.NEW_RELEASE:
-			result += daysRented * 3;
-			break;
-		case Movie.CHILDRENS:
-			result += 1.5;
-			if (daysRented > 3)
-				result += (daysRented - 3) * 1.5;
+	abstract double getCharge(int daysRented);
 
-			break;
-		}
-		return result;
+	int getFrequentRenterPoints(int daysRented) {
+		if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1)
+			return 2;
+		else
+			return 1;
 	}
 }
